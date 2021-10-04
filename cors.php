f0bfd2c8edb3a2b027924edb218535fd359c4614
maparcepel <maparcepel@gmail.com>
2021-10-04 17:22:07 +0200

    ok

--- cors.php
+++ cors.php
@@ -1,5 +1,25 @@
 <?php
-$domain = "http://localhost:3000";
+
+switch($_SERVER['HTTP_ORIGIN']){
+  
+  case 'https://marcelmolina.net':
+    $domain = "https://marcelmolina.net";
+    break;
+
+  case 'http://marcelmolina.net':
+    $domain = "http://marcelmolina.net";
+    break;
+
+  case 'https://marcelmolina.tk':
+    $domain = "https://marcelmolina.tk";
+    break;
+
+  default:
+    $domain = "http://marcelmolina.tk";
+    break;
+}
+
+
 header("Access-Control-Allow-Origin: $domain");
 header("Access-Control-Allow-Headers: content-type");
 header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
--- mailer.php
+++ mailer.php
@@ -1,6 +1,56 @@
 <?php
 include_once "cors.php";
 
+//Import PHPMailer classes into the global namespace
+// //These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-echo json_encode($data);
+
+
+// //Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.marcelmolina.net';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@marcelmolina.net';                     //SMTP username
+    $mail->Password   = '263821Molina';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('info@marcelmolina.net', 'marcelmolina.net');
+    $mail->addAddress('info@marcelmolina.net', 'marcelmolina.net');     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Mensaje desde tu web';
+    $mail->Body    = "<html><body><p>Nombre: " . $data->name . "</p><p>Email: " . $data->email . "</p><p>Mensaje: " . $data->message . "</p></body></html>";
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo json_encode(['Tu mensaje se ha enviado correctamente.']);
+} catch (Exception $e) {
+    echo "Ups. Algo ha fallado. Inténtalo más tarde o envíame un email.";
+}
+
+
